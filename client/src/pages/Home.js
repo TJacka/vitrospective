e2ef1db296a9885a7fcad277a714998acbff2dd9
TJacka <vegozzy@gmail.com>
2022-12-01 14:49:31 -0800

    Updated homepgae button links

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,8 +7,8 @@ const HomePage = () => {
 		      <h1 className='hero-h1'>Artisan Glass Beads & Jewelry</h1>
 	        <h6 className="hero-sub">Exquisite unique pieces handmade from Italian Murano glass.</h6>
           <div className="hero-btns">
-            <Button href="/vitrospective/beads/" className='hero-btn' variant="secondary">Shop Beads</Button>
-            <Button href="/vitrospective/jewelry/" className='hero-btn' variant="dark" style={{backgroundColor:"black", paddingTop: "12px"}}>Shop Jewelry</Button>
+            <Button href="/beads/" className='hero-btn' variant="secondary">Shop Beads</Button>
+            <Button href="/jewelry/" className='hero-btn' variant="dark" style={{backgroundColor:"black", paddingTop: "12px"}}>Shop Jewelry</Button>
           </div>
 	      </div>
       </div>
